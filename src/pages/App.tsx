57400e6587abaf7e47f1f4aabb6217e692032d0f
mohireza <mohireza50@gmail.com>
2026-08-27 09:38:30 +0330

    feat: add text and make sections responsive

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -19,12 +19,22 @@ import hardwareClr from "../assets/section-icons/hardware_color.webp";
 const App = () => {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center w-full">
-        <div className="grid p-4 grid-cols-2 sm:p-20 animate-gap-open scale-20 max-w-150 sm:animate-gap-open-wide">
-          <Section title="About Me" bwSrc={about} clrSrc={aboutClr} />
-          <Section title="Frontend Projects" bwSrc={front} clrSrc={frontClr} />
-          <Section title="DL Projects" bwSrc={ai} clrSrc={aiClr} />
-          <Section title="Hardware" bwSrc={hardware} clrSrc={hardwareClr} />
+      <div className="max-h-screen overflow-hidden flex flex-col items-center justify-center w-full px-2 py-8 sm:p-30">
+        <h1 className="text-2xl font-bold text-center">
+          Hi, I'm Mohammadreza Heydari
+        </h1>
+        <div className="flex flex-col justify-center gap-2 p-4 sm:p-10">
+          <p className="text-center">Here is a collection of my projects</p>
+          <div className="grid grid-cols-2 mt-4 animate-gap-open scale-20 max-w-120 sm:animate-gap-open-wide">
+            <Section title="About Me" bwSrc={about} clrSrc={aboutClr} />
+            <Section
+              title="Frontend Projects"
+              bwSrc={front}
+              clrSrc={frontClr}
+            />
+            <Section title="DL Projects" bwSrc={ai} clrSrc={aiClr} />
+            <Section title="Hardware" bwSrc={hardware} clrSrc={hardwareClr} />
+          </div>
         </div>
       </div>
     </>
