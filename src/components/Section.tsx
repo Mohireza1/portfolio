019c76aa8e36438b00c38496581732bd01706dbf
mohireza <mohireza50@gmail.com>
2026-08-27 17:45:59 +0330

    bug: fix section reponsiveness

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,20 +3,27 @@ type props = { title: string; bwSrc: string; clrSrc: string };
 const Section = ({ title, bwSrc, clrSrc }: props) => {
   return (
     <section className="gridi place-items-center cursor-pointer">
-      <div className="relative group inline-block border-4 sm:border-8 border-gray-100 rounded-xl overflow-hidden">
+      <div
+        className="
+      relative group inline-block border-4 sm:border-0 border-gray-100 rounded-xl overflow-hidden sm:overflow-visible max-w-45
+
+      sm:before:absolute sm:before:w-[105%] sm:before:h-[107%] sm:before:border-2 sm:before:border-gray-100 sm:before:-top-3 sm:before:-left-2 md:before:-left-2.5 before:rounded-xl
+
+      sm:after:absolute sm:after:w-[105%] sm:after:h-[107%] sm:after:border-2 after:border-gray-100 after:-bottom-3 after:rounded-xl"
+      >
         <img
           src={bwSrc}
           alt="Black and white"
-          className="block w-full h-auto"
+          className=" block w-full h-auto sm:rounded-tl-xl sm:rounded-br-xl"
         />
 
         <img
           src={clrSrc}
           alt="Colored"
-          className="absolute top-0 left-0 w-full h-full object-cover transition-all duration-250 sm:duration-150 ease-linear [clip-path:inset(100%_0_0_0)] group-hover:[clip-path:inset(0)]"
+          className="absolute top-0 left-0 w-full h-full object-cover transition-all duration-250 sm:duration-150 ease-linear [clip-path:inset(100%_0_0_0)] group-hover:[clip-path:inset(0)] sm:rounded-tl-xl sm:rounded-br-xl"
         />
       </div>
-      <h2 className="invisible text-center font-medium text-lg sm:text-xl mt-1 animate-blocking">
+      <h2 className="invisible text-center font-medium text-md sm:text-lg mt-1 sm:mt-3 animate-blocking">
         {title}
       </h2>
     </section>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -19,13 +19,15 @@ import hardwareClr from "../assets/section-icons/hardware_color.webp";
 const App = () => {
   return (
     <>
-      <div className="max-h-screen overflow-hidden flex flex-col items-center justify-center w-full px-2 py-8 sm:p-30">
-        <h1 className="text-2xl font-bold text-center">
+      <div className="max-h-screen flex flex-col items-center justify-center w-full py-10 px-1 sm:py-10">
+        <h1 className="text-2xl font-bold text-center mb-4">
           Hi, I'm Mohammadreza Heydari
         </h1>
         <div className="flex flex-col justify-center gap-2 p-4 sm:p-10">
-          <p className="text-center">Here is a collection of my projects</p>
-          <div className="grid grid-cols-2 mt-4 animate-gap-open scale-20 max-w-120 sm:animate-gap-open-wide">
+          <p className="text-center invisible animate-blocking">
+            Here is a collection of my projects
+          </p>
+          <div className="grid grid-cols-2 my-2 sm:mt-8 animate-gap-open scale-20 max-w-90 sm:max-w-120 sm:animate-gap-open-wide">
             <Section title="About Me" bwSrc={about} clrSrc={aboutClr} />
             <Section
               title="Frontend Projects"
